b53aa554ce4a2836b1279aa539f6370dfaeb4cf1
Jimmy Prescott <jimmy.prescott@rackspace.com>
2014-01-10 15:50:08 -0600

    updates

--- cloudfunctions.py
+++ cloudfunctions.py
@@ -94,8 +94,8 @@ def ansibleAdd():
     ansiblegroup = raw_input("What group do you want to add the ansible node to: ")
     print "Adding %s to /etc/ansible/hosts as part of the %s group: " % (ansiblehost, ansiblegroup)
     with open("/etc/ansible/hosts", "a") as anshostfile:
-        anshostfile.write(ansiblegroup"\n")
-        anshostfile.write(ansiblehost"\n")
+        anshostfile.write(ansiblegroup + "\n")
+        anshostfile.write(ansiblehost + "\n")
     raw_input("Press any key to continue: ")
 
 
